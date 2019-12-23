0b0effe34f29408c600cca74bb344660d524a3a5
のびすけ <rsksound@gmail.com>
2019-12-24 02:00:57 +0900

    ミニマム構成に

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let auth0 = null;
 
-const fetchAuthConfig = () => fetch("/auth_config.json");
+const fetchAuthConfig = () => fetch("auth_config.json");
 
 const configureClient = async () => {
   const response = await fetchAuthConfig();
