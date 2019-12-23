70c6caa2354ce618ef14b625371f6e2ccd536b6d
のびすけ <rsksound@gmail.com>
2019-12-24 02:13:19 +0900

    ミニマム構成に

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ const updateUI = async () => {
 
 const login = async () => {
   await auth0.loginWithRedirect({
-    redirect_uri: window.location.origin
+    redirect_uri: window.location.origin + '/auth0staticsitesample'
   });
 };
 
